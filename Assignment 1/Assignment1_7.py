83201d92ef854c6f4cfd6b583d72a9c481886bea
rutujapatil1306 <105958437+rutujapatil1306@users.noreply.github.com>
2024-04-25 22:32:29 +0530

    Update Assignment1_7.py

--- Assignment 1/Assignment1_7.py
+++ Assignment 1/Assignment1_7.py
@@ -1,3 +1,5 @@
+# write a program which contains one function that accept one number from user & return true if number is divisible by 5 otherwise return false 
+
 def Divisible(No):
     if(No % 5 == 0):
         print("True")
